ad85715595b90d0bf14defdd8571b35903f35625
Kyra15 <84361652+Kyra15@users.noreply.github.com>
2023-10-16 00:58:15 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,43 +1,43 @@
-import sqlite3
-import pandas as pd
+# import sqlite3
+# import pandas as pd
 
-def get_inputs():
-    fname = input("First Name: ")
-    while not fname.isalpha():
-        fname = input("First Name: ")
-    lname = input("Last Name: ")
-    while not lname.isalpha():
-        lname = input("First Name: ")
-    usern = input("Username: ")
-    psswd = hash(input("Password: "))
-    return fname, lname, usern, psswd
+# def get_inputs():
+#     fname = input("First Name: ")
+#     while not fname.isalpha():
+#         fname = input("First Name: ")
+#     lname = input("Last Name: ")
+#     while not lname.isalpha():
+#         lname = input("First Name: ")
+#     usern = input("Username: ")
+#     psswd = hash(input("Password: "))
+#     return fname, lname, usern, psswd
 
-def createtable():
-    c.execute('''CREATE TABLE IF NOT EXISTS tanklogin
-            ([user_id] INTEGER PRIMARY KEY AUTOINCREMENT, 
-            [firstname] TEXT, 
-            [lastname] TEXT, 
-            [username] TEXT, 
-            [password] TEXT)
-            ''')
-
-
-def deletetable():
-    c.execute("DROP TABLE tanklogin")
+# def createtable():
+#     c.execute('''CREATE TABLE IF NOT EXISTS tanklogin
+#             ([user_id] INTEGER PRIMARY KEY AUTOINCREMENT, 
+#             [firstname] TEXT, 
+#             [lastname] TEXT, 
+#             [username] TEXT, 
+#             [password] TEXT)
+#             ''')
 
 
-conn = sqlite3.connect('test_database')
-c = conn.cursor()
+# def deletetable():
+#     c.execute("DROP TABLE tanklogin")
 
-createtable()
 
-parameters = get_inputs()
+# conn = sqlite3.connect('test_database')
+# c = conn.cursor()
 
-c.execute('INSERT INTO tanklogin (firstname, lastname, username, password) VALUES (?, ?, ?, ?)', parameters)
-conn.commit()
+# createtable()
 
-c.execute('SELECT * FROM tanklogin')
-df = pd.DataFrame(c.fetchall(), columns=['user_id', 'firstname', 'lastname', 'username', 'password'])
-print(df)
+# parameters = get_inputs()
 
-conn.close()
+# c.execute('INSERT INTO tanklogin (firstname, lastname, username, password) VALUES (?, ?, ?, ?)', parameters)
+# conn.commit()
+
+# c.execute('SELECT * FROM tanklogin')
+# df = pd.DataFrame(c.fetchall(), columns=['user_id', 'firstname', 'lastname', 'username', 'password'])
+# print(df)
+
+# conn.close()
